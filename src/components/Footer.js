33634985edf25778c4514d0a3e24c787a0bb6a8b
y-komatsuz <53844425+y-komatsuz@users.noreply.github.com>
2019-08-11 23:48:01 +0900

    Revert "comment out the products from navibar and footer js"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -34,11 +34,11 @@ const Footer = class extends React.Component {
                         About
                       </Link>
                     </li>
-                    <!--<li>
+                    <li>
                       <Link className="navbar-item" to="/products">
                         Products
                       </Link>
-                    </li>-->
+                    </li>
                     <li>
                       <Link className="navbar-item" to="/contact/examples">
                         Form Examples
